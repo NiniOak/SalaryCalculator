fcbf005cab7945b8f2e84c65dbf85dc966c118f5
Anita Conestoga <anitaconestoga@Sachins-MacBook-Pro.local>
2017-01-23 16:56:30 -0500

    Initial Commit

---
+++ Anita_Assignment1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anita_Assignment1
+//
+//  Created by Anita Conestoga on 2017-01-23.
+//  Copyright © 2017 Anita Conestoga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Anita_Assignment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Anita_Assignment1
+//
+//  Created by Anita Conestoga on 2017-01-23.
+//  Copyright © 2017 Anita Conestoga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
